6d0f7fd2c45b483f5f92501bd8b4c28ce6da1e7c
Oscar <oooosssososos@hotmail.com>
2014-03-15 14:05:55 -0700

    first

---
+++ src/com/example/untitled4/MyActivity.java
@@ -0,0 +1,17 @@
+package com.example.untitled4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start);
+    }
+}

fc7a66fb2cea27e8b5c56533237043569c3fb31a
Oscar <oooosssososos@hotmail.com>
2014-03-15 16:11:32 -0700

    added more screens

--- src/com/example/untitled4/MyActivity.java
+++ src/com/example/untitled4/MyActivity.java
@@ -2,6 +2,8 @@ package com.example.untitled4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
@@ -13,6 +15,12 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start);
-        R.id.
+        findViewById(R.id.imageButton).setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Inform the user the button has been clicked
+                setContentView(R.layout.main);
+            }
+        });
     }
 }

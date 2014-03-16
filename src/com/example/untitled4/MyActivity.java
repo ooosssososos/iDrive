8f83f920c3a20b19a38eada0e1dc95cc035f4e0c
Kyle Chan <kylechan1112@gmail.com>
2014-03-15 18:46:39 -0700

    HTTP Post & JSON

--- src/com/example/untitled4/MyActivity.java
+++ src/com/example/untitled4/MyActivity.java
@@ -4,8 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class MyActivity extends Activity {
     /**
@@ -13,6 +21,10 @@ public class MyActivity extends Activity {
      */
     public static String Username = "";
     public static int code = 0;
+    private static String KEY_SUCCESS = "success";
+    private static String KEY_ERROR = "error";
+    private static String KEY_ERROR_MSG = "error_msg";
+    private static String KEY_UID = "uid";
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -22,7 +34,12 @@ public class MyActivity extends Activity {
         bu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Username = ((EditText)findViewById(R.id.editText)).getText().toString();;
+                Username = ((EditText)findViewById(R.id.editText)).getText().toString();
+                try {
+                    postData();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 setContentView(R.layout.main);
                 ((TextView)findViewById(R.id.textView)).setText(Username + "");
                 register();
@@ -30,6 +47,32 @@ public class MyActivity extends Activity {
         });
 
     }
+
+    private JSONObject jsonResult(String Name) throws JSONException {
+        JSONObject json = null;
+        json = new JSONObject("{\"" + "name" + "\":" + "\"" + Name+ "\"" + "}");
+        return json;
+    }
+
+        public void postData() throws JSONException {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://idrivedjango-env-qrs5vkxvvi.elasticbeanstalk.com/api/partyuser/");
+        Username = ((EditText)findViewById(R.id.editText)).getText().toString();
+        try {
+            // Add your data
+            jsonResult(Username);
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+    }
+
     public void register(){
         ((ImageButton)findViewById(R.id.imageButton)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +96,8 @@ public class MyActivity extends Activity {
             public void onClick(View v) {
                 setContentView(R.layout.lobby);
                 //Fill lobby with info TODO
+
+
             }
         });
     }

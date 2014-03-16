7ceb2714e3e07b6da39edb96bc2929d070dda643
Oscar <oooosssososos@hotmail.com>
2014-03-15 20:46:38 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/workspace.xml

--- src/com/example/untitled4/Post.java
+++ src/com/example/untitled4/Post.java
@@ -7,12 +7,18 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * Created by ics on 15/03/14.
@@ -25,35 +31,43 @@ public class Post extends AsyncTask<Instruction, Integer, Long>{
     @Override
     protected Long doInBackground(Instruction... i) {
         for(Instruction c : i){
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost(c.a);
-        c.d = d.getText().toString();
-        try {
-            // Add your data
-            jsonResult(c.d);
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(c.a);
+            c.d = d.getText().toString();
+            try {
+                // Add your data
+                StringEntity e = new StringEntity(jsonResult(c.d).toString());
+                httppost.setEntity(e);
 
-            // Execute HTTP Post Request
-            HttpResponse response = httpclient.execute(httppost);
 
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+                // Execute HTTP Post Request
+                httppost.addHeader("content-type", "application/json");
+                HttpResponse response = httpclient.execute(httppost);
+                System.out.println("Status :" + response.getStatusLine());
+
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
         return 1L;
     }
+
+
     private JSONObject jsonResult(String Name) throws JSONException {
-        JSONObject json = new JSONObject();
-        int[] past_parties = new int[1];
-        int[] friends = new int[1];
-        json.put("cur_party", 1);
-        json.put("past_parties", past_parties);
-        json.put("friends", friends);
-        json.put("name", Name);
-        System.out.println(json.toString());
+        JSONObject json = null;
+        Object cur_party = JSONObject.NULL;
+        int past_parties[] = {1};
+        int friends[] = {1};
+        json = new JSONObject();
+        json.put("cur_party",cur_party);
+        json.put("past_parties",new JSONArray(past_parties));
+        json.put("name",Name);
+        json.put("friends",new JSONArray(friends));
+        System.out.println("Json: " + json);
         return json;
     }
 }

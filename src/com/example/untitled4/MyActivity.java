7e905f4daa1c53f95b1c7cbd6c971c1d53406d24
Oscar <oooosssososos@hotmail.com>
2014-03-15 17:38:46 -0700

    added more screens

--- src/com/example/untitled4/MyActivity.java
+++ src/com/example/untitled4/MyActivity.java
@@ -3,9 +3,7 @@ package com.example.untitled4;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.*;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
@@ -13,21 +11,56 @@ public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+    public static String Username = "";
+    public static int code = 0;
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start);
         Button bu = (Button)findViewById(R.id.button);
-        System.out.println("num = " + R.id.button);
-        if(bu == null)System.out.println("nullarasdasdadwa");
-        else{
-            System.out.println("notnull");
-        }
         bu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Inform the user the button has been clicked
+                Username = ((EditText)findViewById(R.id.editText)).getText().toString();;
                 setContentView(R.layout.main);
+                ((TextView)findViewById(R.id.textView)).setText(Username + "");
+                register();
+            }
+        });
+
+    }
+    public void register(){
+        ((ImageButton)findViewById(R.id.imageButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.lobby);
+                ((TextView)findViewById(R.id.textView)).setText(Username + "'s ");
+                registerLobby();
+            }
+        });
+        ((ImageButton)findViewById(R.id.imageButton2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.join);
+                registerJoin();
+            }
+        });
+    }
+    public void registerJoin(){
+        ((Button)findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.lobby);
+                //Fill lobby with info TODO
+            }
+        });
+    }
+    public void registerLobby(){
+        ((Button)findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.modes);
             }
         });
     }
